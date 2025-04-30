57688bdaf89d426f6c9f4aa11908dd89787b0332
samane ghodrati <samane.ghodrati@gmail.com>
2025-04-30 00:00:18 -0900

    add

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
 const app=express();
+app.use(express.json())
 const port=process.env.port || 3000;
 const mockUser=[ 
     {id:1, usename:"anson",displayname:"Anson"},
     {id:2, usename:"jack",displayname:"jack"},
-    {id:3, usename:"adam",displayname:"Adam"}
+    {id:3, usename:"aziz",displayname:"azizi"},
+    {id:4, usename:"akbar",displayname:"Akbari"},
+    {id:5, usename:"asghar",displayname:"Asghari"}
 ]
 app.get('/',(req,res)=>{
     res.status(201).send({msg:'hello '})
@@ -12,7 +15,12 @@ app.get('/',(req,res)=>{
 app.get('/api/users',(req,res)=>{
     console.log(req.query);
     const{query:{filter,value},}=req;
-if(!filter&&!value) return res.send(mockUser);
+// if(!filter&&!value) return res.send(mockUser);
+if(!filter&&!value) return res.send(
+    mockUser.filter(user=>user[filter].includes(value))
+);
+return res.send(mockUser);
+
     // res.send([
     //     {id:1, usename:"anson",displayname:"Anson"},
     //     {id:2, usename:"jack",displayname:"jack"},
@@ -20,14 +28,23 @@ if(!filter&&!value) return res.send(mockUser);
 
     // ])
 })
-app.get('/api/users',(req,res)=>{
-    res.send([
-        {id:1, usename:"anson",displayname:"Anson"},
-        {id:2, usename:"jack",displayname:"jack"},
-        {id:3, usename:"adam",displayname:"Adam"}
+// app.get('/api/users',(req,res)=>{
+//     res.send([
+//         {id:1, usename:"anson",displayname:"Anson"},
+//         {id:2, usename:"jack",displayname:"jack"},
+//         {id:3, usename:"adam",displayname:"Adam"}
 
-    ])
+//     ])
+// })
+app.post('/api/user',(req,res)=>{
+    const {body}=req.body;
+ const newUser = {
+    id:mockUser.length+1,
+    ...body
+};
+return res.status(201).send(newUser)
 })
+
 app.get('/api/users/:id',(req,res)=>{
     console.log(req.params);
     const id=parseInt(req.params.id)
@@ -40,6 +57,33 @@ if(!finduser) return res.sendStatus(404)
 app.get('/api/product',(req,res)=>{
     res.send([{id:123,name:"chicken",price:12.99}])
 })
+
+app.put('/api/users/:id',(req,res)=>{
+    const {body,params:{id}}=req;
+    const parseId=parseInt(id);
+    if(isNaN(parseId)) return res.status(400);
+
+     const findUserIndex= mockUser.findIndex(user => user.id===parseId);
+     if(findUserIndex===-1) return res.send(404);
+     mockUser[findUserIndex]={
+        id:parseId,
+        ...body
+     };
+     return res.status(200);
+});
+app.patch('/api/users/:id',(req,res)=>{
+    const {body,params:{id}}=req;
+    const parseId=parseInt(id);
+    if(isNaN(parseId)) return res.status(400);
+
+     const findUserIndex= mockUser.findIndex(user => user.id===parseId);
+     if(findUserIndex===-1) return res.send(404);
+     mockUser[findUserIndex]={
+        ...mockUser[findUserIndex],
+        ...body
+     };
+     return res.status(200);
+});
 app.listen(port,()=>{
     console.log(`ruuning on port ${port}`);
     
